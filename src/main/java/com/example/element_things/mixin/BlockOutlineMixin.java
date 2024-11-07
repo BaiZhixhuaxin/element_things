7ef4b033e1bd532e4fcda4e89a4525c00be37577
BaiZhihuaxin <2662629773@qq.com>
2024-11-07 19:09:45 +0800

    1

---
+++ src/main/java/com/example/element_things/mixin/BlockOutlineMixin.java
@@ -0,0 +1,46 @@
+package com.example.element_things.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.resource.SynchronousResourceReloader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.shape.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WorldRenderer.class)
+public abstract class BlockOutlineMixin implements SynchronousResourceReloader , AutoCloseable {
+    @Shadow @Nullable private ClientWorld world;
+
+    @Inject(method = "drawBlockOutline",at=@At("HEAD"), cancellable = true)
+    private void drawBlockOutline(MatrixStack matrices, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos pos, BlockState state, CallbackInfo ci) {
+        drawCuboidShapeOutline(matrices, vertexConsumer, state.getOutlineShape(world, pos, ShapeContext.of(entity)), (double)pos.getX() - cameraX, (double)pos.getY() - cameraY, (double)pos.getZ() - cameraZ, 29 / 255F, 105 / 255F, 161 / 255F, 1.0F);
+        ci.cancel();
+    }
+    @Unique
+    private static void drawCuboidShapeOutline(MatrixStack matrices, VertexConsumer vertexConsumer, VoxelShape shape, double offsetX, double offsetY, double offsetZ, float red, float green, float blue, float alpha) {
+        MatrixStack.Entry entry = matrices.peek();
+        shape.forEachEdge((minX, minY, minZ, maxX, maxY, maxZ) -> {
+            float k = (float)(maxX - minX);
+            float l = (float)(maxY - minY);
+            float m = (float)(maxZ - minZ);
+            float n = MathHelper.sqrt(k * k + l * l + m * m);
+            k /= n;
+            l /= n;
+            m /= n;
+            vertexConsumer.vertex(entry, (float)(minX + offsetX), (float)(minY + offsetY), (float)(minZ + offsetZ)).color(red, green, blue, alpha).normal(entry, k, l, m);
+            vertexConsumer.vertex(entry, (float)(maxX + offsetX), (float)(maxY + offsetY), (float)(maxZ + offsetZ)).color(red, green, blue, alpha).normal(entry, k, l, m);
+        });
+    }
+}
